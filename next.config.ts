63e3287af624fa450ecf837a207ce6ae2920e4e9
Praneshxo <praneshgara@gmail.com>
2026-04-12 15:21:15 +0530

    ui pending

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.194.2.250'],
-  turbopack: {
-    root: __dirname,
+  experimental: {
+    turbopack: {
+      root: __dirname,
+    },
   },
 };
 
